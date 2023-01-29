6403bfcb4960dad9fb3829fc486ee5a273ed9b39
EMCain <11471197+EMCain@users.noreply.github.com1>
2023-01-29 14:36:41 -0800

    Create page outline

---
+++ notes.md
@@ -0,0 +1,7 @@
+CSS grid reference: https://www.w3schools.com/css/tryit.asp?filename=trycss_grid
+
+SCSS:
+
+https://vitejs.dev/guide/features.html#css-modules
+
+https://css-tricks.com/how-to-import-a-sass-file-into-every-vue-component-in-an-app/
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,32 +7,42 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+
+<div class="grid-container">
+  <div class="header">
+    <p>A demo of recursion and nested subcategories</p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div class="menu">Menu</div>
+  <div class="left">Category List</div>  
+  <div class="right">Select Items</div>
+  <div class="footer">Today's Shopping List</div>
+</div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.header { grid-area: header; }
+.menu { grid-area: menu; }
+.left { grid-area: main; }
+.right { grid-area: right; }
+.footer { grid-area: footer; }
+
+.grid-container {
+  display: grid;
+  grid-template-areas:
+    'header header header header header header'
+    'menu main main main right right'
+    'menu footer footer footer footer footer';
+  gap: 10px;
+  background-color: #2196F3;
+  padding: 10px;
+  width: 90vw;
+}
+
+.grid-container > div {
+  background-color: rgba(255, 255, 255, 0.8);
+  text-align: center;
+  padding: 20px 0;
+  font-size: 30px;
 }
 </style>
